3b164bdad6734c9f362e6641e5a261a762963d33
Rutuja-Maurya <rutujamaurya0707@gmail.com>
2022-03-07 18:59:34 +0530

    Exception handling task done

---
+++ IndexOutOfrangeExceptionDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exceptionhandling
+{
+    public class IndexOutOfrangeExceptionDemo
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 19, 0, 75, 52 };
+
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[7]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(" {0}", arr[i]);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Nullreferenceexceptiondemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionHandling2
+{
+    class Nullreferenceexceptiondemo
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            try
+            {
+                Console.WriteLine(str.Substring(5));
+            }
+            catch (NullReferenceException err)
+            {
+                Console.WriteLine("Please check the string str.");
+                Console.WriteLine(err.Message);
+            }
+            Console.WriteLine("Continuing with other statments..");
+        }
+    }
+}
