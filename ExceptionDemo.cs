28ce4c39435ed55bbeb65183d6e2b3d495204a6b
Rutuja-Maurya <rutujamaurya0707@gmail.com>
2022-03-07 10:24:41 +0530

    task submitted

---
+++ ExceptionDemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionDemoo
+{
+    public class ExceptionDemo
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 19, 0, 75, 52 };
+
+            try
+            {
+
+                // Try to generate an exception
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[7]);
+                }
+            }
+
+            // Catch block for invalid array access
+            catch (IndexOutOfRangeException e)
+            {
+
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+                FileStream fs = new FileStream("D:\\filedemo.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(e.Message);
+                sw.Close();
+                fs.Close();
+            }
+
+            // Catch block for attempt to divide by zero
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+            }
+
+            // Catch block for value being out of range
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+            }
+
+            // Finally block
+            // Will execute irrespective of the above catch blocks
+            finally
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(" {0}", arr[i]);
+                }
+            }
+        }
+    }
+}
